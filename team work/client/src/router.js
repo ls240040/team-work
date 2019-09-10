8d256c1c7ad50f2ddd4d40ed3d7c78d7289b9be4
ls240040 <ls326483836@163.com>
2019-09-10 20:23:36 +0800

    u

--- team work/client/src/router.js
+++ team work/client/src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Index from './views/Index.vue'
+import Details from './views/Details.vue'
+import Community from './views/Community.vue'
+import Mine from './views/Mine.vue'
+import Vip from './views/Vip.vue'
 
 Vue.use(Router)
 
@@ -8,16 +12,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'index',
+      component: Index
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/Details',
+      name: 'details',
+      component: Details
+    },
+    {
+      path: '/Community',
+      name: 'community',
+      component: Community
+    },
+    {
+      path: '/Mine',
+      name: 'mine',
+      component: Mine
+    },
+    {
+      path: '/Vip',
+      name: 'vip',
+      component: Vip
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
