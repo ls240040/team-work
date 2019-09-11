3200bf0ea8ef9253511036345dfbcad584f327c6
ls240040 <ls326483836@163.com>
2019-09-11 09:15:11 +0800

    u

---
+++ team work/client/src/components/Community/communityRecommend.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <mt-swipe :auto="4000">
+            <mt-swipe-item class="swipe">1111</mt-swipe-item>
+            <mt-swipe-item class="swipe">2222</mt-swipe-item>
+            <mt-swipe-item class="swipe">3333</mt-swipe-item>
+            <mt-swipe-item class="swipe">4444</mt-swipe-item>
+        </mt-swipe>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .mint-swipe{
+            width:100%;
+            height:15rem;
+            background: black
+        }
+        .mint-swipe >>> .mint-swipe-indicator{
+            width: 44px;
+            height: 4px;
+        }
+        .swipe .mint-swipe-indicator{
+            background: rgb(255, 0, 0);
+        }
+</style>
