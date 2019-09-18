45125ca52b90bc2c4831b6122f1e96632a3b03bc
wzsyy <15958336931@163.com>
2019-09-18 20:15:13 +0800

    1

--- team work/client/src/router.js
+++ team work/client/src/router.js
@@ -28,7 +28,10 @@ export default new Router({
     routes: [{
             path: '/menu',
             name: 'menu',
-            component: Menu
+            component: Menu,
+            meta: {
+                isLogin: true // 添加该字段，表示进入这个路由是需要登录的
+            }
         },
         {
             path: '/vipcarousel',
