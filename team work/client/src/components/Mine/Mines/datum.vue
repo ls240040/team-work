d628176d68a21d0637f5535d77f4d31d0b88ed39
xiao <xiao@qq.com>
2019-09-18 09:11:53 +0800

    datum

--- team work/client/src/components/Mine/Mines/datum.vue
+++ team work/client/src/components/Mine/Mines/datum.vue
@@ -22,7 +22,7 @@
         <mt-cell title="真实姓名" is-link>
           <span>真实姓名</span>
         </mt-cell>
-        <mt-cell title="性别" is-link>
+        <mt-cell id="btn_show" title="性别" is-link>
           <span>性别</span>
         </mt-cell>
         <mt-cell title="生日" is-link>
@@ -44,6 +44,15 @@
       <mt-cell title="邮箱" is-link>
         <span>邮箱</span>
       </mt-cell>
+      <div class="sex" id="blocks" style="display:none;">      
+          <div>
+              <span id="btn_hidden">取消</span>
+              <span class="sexs">性别</span>
+              <span class="ok">确认</span>
+          </div>
+          <h3 class="man">男</h3>   
+          <h3 class="woman">女</h3>   
+      </div>
   </div>
 </template>
 <script>
@@ -53,10 +62,26 @@ export default {
   },
   methods: {}
 };
+  window.onload = function () {
+      /*获取blocks="id"给变量 blocks*/
+        var blocks = document.getElementById("blocks");
+         /*获取显示按钮id="btn_show"给变量 btn_show*/
+        var btn_show = document.getElementById("btn_show");
+
+       /*获取隐藏按钮id=“btn_show”给变量 btn_hidden */
+        var btn_hidden = document.getElementById("btn_hidden");
+        /*点击性别时，blocks的样式style的display属性赋值为“block”*/
+        btn_show.onclick = function () {
+            blocks.style.display = "block";
+        }
+        /*点击取消时，blocks的样式style的display属性赋值为“none”*/        
+        btn_hidden.onclick = function () {
+            blocks.style.display = "none";
+        }
+  }
 </script>
 
-<style lang="scss">
-    
+<style lang="scss">    
     
     .datum{
         background:#eee;   
@@ -89,6 +114,30 @@ export default {
         img{
           width:0.4rem;height:0.4rem;
         }
-    }
+        .sex{
+            width:100%;
+            border-top:0.01rem solid #ddd;
+            position:absolute;
+            bottom:0rem;
+            background:#fff;
+            div{
+                font-size:0.3rem;
+                display: flex;
+                justify-content: space-around;
+                padding:0.3rem;
+                margin-bottom:0.5rem;
+                .sexs{
+                    font-size:0.4rem;
+                }
+                .ok{color:#f00;}
+              }
+              h3{
+                  font-size:0.5rem;
+                  padding:0.2rem;              
+              }
+              
+              h3:hover{background:#eae0e3;}
+        }
+   }
 </style>
 
