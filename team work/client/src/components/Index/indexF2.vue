df41a5bf86acb0446541f23b94bdab068fd3a99d
wzsyy <15958336931@163.com>
2019-09-19 15:45:39 +0800

    1

--- team work/client/src/components/Index/indexF2.vue
+++ team work/client/src/components/Index/indexF2.vue
@@ -5,7 +5,7 @@
       <p>排号</p>
       <span>取号快人一步</span>
     </div>
-    <div class="middle">
+    <div class="middle" @click="linkTo">
       <p>预订</p>
       <span>早预定不等位</span>
     </div>
@@ -21,7 +21,9 @@ export default {
     return {};
   },
   methods: {
-   
+    linkTo(){
+      this.$router.push("/shopList");
+    },
   }
 };
 </script>
