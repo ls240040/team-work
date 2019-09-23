cb136989ccfe2dc232cfcc2687544c031f11ed9d
wzsyy <15958336931@163.com>
2019-09-23 09:22:20 +0800

    1

--- team work/client/src/components/Index/indexShop.vue
+++ team work/client/src/components/Index/indexShop.vue
@@ -1,44 +1,69 @@
 <!--index.vue 商品组件-->
 <template>
   <div class="shop">
-    <div>
-      <img src="http://127.1:5050/index/sp1.png" alt>
-      <p class="shopTitle">招牌虾滑</p>
-      <p><span>580捞币</span>&nbsp;兑换59813</p>
+    <div v-for="(item,index) in list1" :key="index">
+      <img :src="`http://127.1:5050${item.S_Href}`" alt>
+      <p class="shopTitle">{{item.S_Title}}</p>
+      <p>
+        <span>{{item.S_price}}捞币</span>&nbsp;兑换{{item.S_Count}}
+      </p>
     </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      list1: []
+    };
   },
-  methods: {}
+  methods: {
+    loadimg() {
+      var url = "index/indexShop";
+      this.axios.get(url).then(res => {
+        if (res.data.code == 1) {
+          this.list1 = res.data.data;
+          console.log(this.list1);
+        }
+      });
+    }
+  },
+  created() {
+    this.loadimg();
+  }
 };
 </script>
 
 <style lang="scss">
-    .shop{
-        text-align: left;
-        margin-top:.3rem; 
-        img{
-            width:3rem;
-            border-radius: .1rem;
-        }
-        p{
-            margin-top: .2rem;
-            font-size: .3rem;
-            color: #999;
-            span{
-                color: #CFB53B;
-                font-weight: 600;
-            }
-        }
-        p.shopTitle{
-            font-size: .4rem;
-            color: #000;
-        }
-
+.shop {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  text-align: left;
+  margin-top: 0.3rem;
+  img {
+    width: 3rem;
+    border-radius: 0.1rem;
+  }
+  p {
+    margin-top: 0.2rem;
+    font-size: 0.3rem;
+    color: #999;
+    span {
+      color: #cfb53b;
+      font-weight: 600;
     }
+  }
+  p.shopTitle {
+    font-size: 0.4rem;
+    color: #000;
+    width: 2.5rem;
+    display: block;
+    overflow: hidden;
+    word-break: keep-all;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+}
 </style>
 
