d78cbbe4d7a601b18c0d9fe76cff064374342645
EvanFried <359978183@qq.com>
2019-09-24 16:44:08 +0800

    6

--- team work/client/src/components/Index/index02/cityStore.vue
+++ team work/client/src/components/Index/index02/cityStore.vue
@@ -1,121 +1,133 @@
 <template>
-    <div class="cityStore">
-        <mt-header class="myheader" title="城市列表">
-            <router-link to="/storeList" slot="left">
-                <mt-button icon="back"></mt-button>
-            </router-link>
-        </mt-header>  
-         <div class="location">
-            <span>杭州</span>
-            <img src="http://127.0.0.1:5050/icon/row_num_posi.png" alt="">
-            <input type="text" placeholder="请输入外送门店">
-         </div>   
-         <h5>热门城市</h5>            
-        <div class="city">
-            <span>北京</span>
-            <span>上海</span>
-            <span>广州</span>
-            <span>深圳</span>
-            <span>天津</span>
-            <span>杭州</span>
-            <span>成都</span>
-        </div>       
-        <h5>ABCDEF</h5>            
-        <div class="city">
-            <span>北京</span>
-            <span>保定</span>
-            <span>鄂尔多斯</span>
-            <span>大连</span>
-            <span>常州</span>
-            <span>蚌埠</span>
-            <span>福州</span>
-            <span>德州</span>
-            <span>长沙</span>
-            <span>佛山</span>
-            <span>东莞</span>
-            <span>成都</span>
-		</div>
-         <h5>GHIJKL</h5>            
-        <div class="city">
-            <span>邯郸</span>
-            <span>廊坊</span>
-            <span>临汾</span>
-            <span>哈尔滨</span>
-            <span>连云港</span>
-            <span>淮安</span>
-            <span>杭州</span>
-            <span>嘉兴</span>
-            <span>合肥</span>
-            <span>淮北</span>
-            <span>九江</span>
-            <span>济南</span>
-            <span>济宁</span>
-        </div>
+  <div class="cityStore" @click="choosenCity($event)">
+    <mt-header class="myheader" title="城市列表">
+      <router-link to="/storeList" slot="left">
+        <mt-button icon="back"></mt-button>
+      </router-link>
+    </mt-header>
+    <div class="location">
+      <span>{{chooseCity}}</span>
+      <img src="http://127.0.0.1:5050/icon/row_num_posi.png" alt />
+      <input type="text" placeholder="请输入外送门店" />
     </div>
+    <h5>热门城市</h5>
+    <div class="city">
+      <span>北京</span>
+      <span>上海</span>
+      <span>广州</span>
+      <span>深圳</span>
+      <span>天津</span>
+      <span>杭州</span>
+      <span>成都</span>
+    </div>
+    <h5>ABCDEF</h5>
+    <div class="city">
+      <span>北京</span>
+      <span>保定</span>
+      <span>鄂尔多斯</span>
+      <span>大连</span>
+      <span>常州</span>
+      <span>蚌埠</span>
+      <span>福州</span>
+      <span>德州</span>
+      <span>长沙</span>
+      <span>佛山</span>
+      <span>东莞</span>
+      <span>成都</span>
+    </div>
+    <h5>GHIJKL</h5>
+    <div class="city">
+      <span>邯郸</span>
+      <span>廊坊</span>
+      <span>临汾</span>
+      <span>哈尔滨</span>
+      <span>连云港</span>
+      <span>淮安</span>
+      <span>杭州</span>
+      <span>嘉兴</span>
+      <span>合肥</span>
+      <span>淮北</span>
+      <span>九江</span>
+      <span>济南</span>
+      <span>济宁</span>
+    </div>
+  </div>
 </template>
 <script>
 //负责引入store.vue 子组件
 import store from "./store";
 
+
+// Vue.prototype.globalClick = function (callback) {
+//     document.querySelectorAll('span').onclick = function () {
+//         alert("?");
+//     };
+// };
+
 export default {
-    data () {
-        return {
-            selected: 'waisong'
-        }
-    },
-    methods:{ },
-    components: {
-    store: store,
+  data() {
+    return {
+      selected: "waisong",
+      chooseCity:this.$route.query.city,
+    };
   },
-}
+  methods: {
+      choosenCity(e){
+          this.$router.push({ path: "storeList", query: { city: e.target.innerHTML } });
+      }
+  },
+  components: {
+    store: store
+  }
+};
 </script>
 <style lang="scss">
-    
-    .cityStore{
-        .myheader{
-            height:1.2rem;
-            font-size:0.4rem;
-            border-bottom:1px solid #ccc;
-            padding:0.4rem 0 0.2rem 0.2rem;
-        }
-        .location{
-        font-size:0.3rem;
-        background:#fff;
-        margin-top:0.2rem;
-        padding:0.15rem 0.2rem;
-        img{
-            width:0.5rem;height:0.5rem;
-            vertical-align:middle;
-            margin-right:0.3rem;
-        }
-        input{
-            width:70%;
-            border:none;
-            background:#e9e9e9;
-            padding:0.1rem 0.2rem;
-            border-radius:0.05rem;
-        }
-        }
-        h5{
-            font-size:0.3rem;
-            text-align: left;
-            padding:0.2rem;
-            background:#eee;
-        }
-        .city{
-            font-size:0.3rem;
-            text-align: left;
-            border:0.01rem solid #eee;
-            span{
-                display:inline-block;
-                width:33.06%;
-                text-align: center;
-                line-height:330%;
-                border:0.01rem solid #ddd;
-            }
-            span:hover{
-                background:#f2e3e3;
-            }
-        }
+.cityStore {
+  .myheader {
+    height: 1.2rem;
+    font-size: 0.4rem;
+    border-bottom: 1px solid #ccc;
+    padding: 0.4rem 0 0.2rem 0.2rem;
+  }
+  .location {
+    font-size: 0.3rem;
+    background: #fff;
+    margin-top: 0.2rem;
+    padding: 0.15rem 0.2rem;
+    img {
+      width: 0.5rem;
+      height: 0.5rem;
+      vertical-align: middle;
+      margin-right: 0.3rem;
     }
+    input {
+      width: 70%;
+      border: none;
+      background: #e9e9e9;
+      padding: 0.1rem 0.2rem;
+      border-radius: 0.05rem;
+    }
+  }
+  h5 {
+    font-size: 0.3rem;
+    text-align: left;
+    padding: 0.2rem;
+    background: #eee;
+  }
+  .city {
+    font-size: 0.3rem;
+    text-align: left;
+    border: 0.01rem solid #eee;
+    span {
+      display: inline-block;
+      width: 33.06%;
+      text-align: center;
+      line-height: 330%;
+      border: 0.01rem solid #ddd;
+    }
+    span:hover {
+      background: #f2e3e3;
+    }
+  }
+}
 </style>
--- team work/client/src/components/Index/index02/storeList.vue
+++ team work/client/src/components/Index/index02/storeList.vue
@@ -1,86 +1,156 @@
 <template>
-    <div class="storeList">
-        <mt-header class="myheader" title="门店列表">
-            <router-link to="/" slot="left">
-                <mt-button icon="back"></mt-button>
-            </router-link>
-        </mt-header>  
-        <mt-navbar v-model="selected">
-        <mt-tab-item id="waisong">外送</mt-tab-item>
-        <mt-tab-item id="myself">自提</mt-tab-item>
-        </mt-navbar>
-        <!-- tab-container -->
-        <mt-tab-container v-model="selected">
-            <mt-tab-container-item id="waisong">
-                <div class="location">
-                    <span  @click="cityStore">
-                        <span>杭州</span>
-                    <img src="http://127.0.0.1:5050/icon/xiala.png" alt="">
-                    </span>                    
-                    <input type="text" placeholder="请输入外送门店">
-                </div>
-                <!-- 引入门店列表组件 -->
-               <store></store>
-            </mt-tab-container-item>
-            <mt-tab-container-item id="myself">
-                <div class="location">
-                    <span  @click="cityStore">
-                        <span>杭州</span>
-                    <img src="http://127.0.0.1:5050/icon/xiala.png" alt="">
-                    </span>                    
-                    <input type="text" placeholder="请输入自提门店">
-                </div>
-                <!-- 引入门店列表组件 -->
-                <store></store>
-            </mt-tab-container-item>            
-        </mt-tab-container>
-
+  <div class="storeList">
+    <mt-header class="myheader" title="门店列表">
+      <router-link to="/" slot="left">
+        <mt-button icon="back"></mt-button>
+      </router-link>
+    </mt-header>
+    <mt-navbar v-model="selected">
+      <mt-tab-item id="waisong">外送</mt-tab-item>
+      <mt-tab-item id="myself">自提</mt-tab-item>
+    </mt-navbar>
+    <!-- tab-container -->
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="waisong">
+        <div class="location">
+          <span @click="cityStore">
+            <span>{{city}}</span>
+            <img src="http://127.0.0.1:5050/icon/xiala.png" alt />
+          </span>
+          <input type="text" placeholder="请输入外送门店" />
+        </div>
+        <!-- 引入门店列表组件 -->
+        <!-- 生成店面 -->
+        <div class="store">
+          <div class="div1">
+            <img src="http://127.0.0.1:5050/icon/login_logo_img.png" alt />
+            <div class="div2">
+              <h4>海底捞火锅外送(庆春路店)</h4>
+              <span class="span1">营业中</span>
+              <span>湖滨</span>
+            </div>
+            <span class="span2">≤4.7km</span>
+          </div>
+        </div>
         
-    </div>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="myself">
+        <div class="location">
+          <span @click="cityStore">
+            <span>{{city}}</span>
+            <img src="http://127.0.0.1:5050/icon/xiala.png" alt />
+          </span>
+          <input type="text" placeholder="请输入自提门店" />
+        </div>
+        <!-- 引入门店列表组件 -->
+
+        <!-- 生成店面 -->
+        <div class="store">
+          <div class="div1">
+            <img src="http://127.0.0.1:5050/icon/login_logo_img.png" alt />
+            <div class="div2">
+              <h4>海底捞火锅外送(庆春路店)</h4>
+              <span class="span1">营业中</span>
+              <span>湖滨</span>
+            </div>
+            <span class="span2">≤4.7km</span>
+          </div>
+        </div>
+
+      </mt-tab-container-item>
+    </mt-tab-container>
+  </div>
 </template>
 <script>
-//负责引入store.vue 子组件
-import store from "./store";
 
 export default {
-    data () {
-        return {
-            selected: 'waisong'
-        }
-    },
-    methods:{
-        cityStore(){
-            this.$router.push('/cityStore')
-        }        
-     },
-    components: {
-    store: store,
+  data() {
+    return {
+      selected: "waisong",
+      city: "杭州"
+    };
   },
-}
+  methods: {
+    //加载city
+    loadCity() {
+      let c = this.$route.query.city;
+      if (c != null) {
+        this.city = c;
+      }
+    },
+
+    //切换城市
+    cityStore() {
+      this.$router.push({ path: "/cityStore", query: { city: this.city } });
+    }
+  },
+  created() {
+    this.loadCity();
+  },
+};
 </script>
 <style lang="scss">
-    .location{
-        font-size:0.3rem;
-        background:#fff;
-        margin-top:0.2rem;
-        padding:0.15rem 0.2rem;
-        img{
-            vertical-align:middle;
-            margin-right:0.3rem;
-        }
-        input{
-            border:none;
-            background:#e9e9e9;
-            padding:0.1rem 0.2rem;
-            border-radius:0.05rem;
-        }
+.location {
+  font-size: 0.3rem;
+  background: #fff;
+  margin-top: 0.2rem;
+  padding: 0.15rem 0.2rem;
+  img {
+    vertical-align: middle;
+    margin-right: 0.3rem;
+  }
+  input {
+    border: none;
+    background: #e9e9e9;
+    padding: 0.1rem 0.2rem;
+    border-radius: 0.05rem;
+  }
+}
+.storeList {
+  .myheader {
+    height: 1.2rem;
+    font-size: 0.4rem;
+    border-bottom: 1px solid #ccc;
+    padding: 0.4rem 0 0.2rem 0.2rem;
+  }
+}
+
+.store {
+  .div1 {
+    display: flex;
+    justify-content: space-around;
+    padding: 0.3rem;
+    background: #fff;
+    font-size: 0.3rem;
+    border-top: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
+    img {
+      width: 0.7rem;
+      height: 0.7rem;
     }
-    .storeList{
-        .myheader{
-            height:1.2rem;
-            font-size:0.4rem;
-            border-bottom:1px solid #ccc;
-            padding:0.4rem 0 0.2rem 0.2rem;
-        }
+    .div2 {
+      text-align: left;
+      h4 {
+        margin-bottom: 0.2rem;
+      }
+      .span1 {
+        color: #fff;
+        font-size: 0.1rem;
+        background: #23a6ff;
+        border-radius: 0.5rem;
+        padding: 0.03rem 0.08rem;
+        margin-right: 0.2rem;
+      }
+      .span1 + span {
+        color: #aaa;
+      }
     }
+    .span2 {
+      color: #aaa;
+      position: relative;
+      top: 50%;
+      transform: translateY(50%);
+    }
+  }
+}
 </style>
