9ec047797b3a15df5fafd0e1bbfa3e5f40e723eb
wzsyy <15958336931@163.com>
2019-09-25 17:37:08 +0800

    1

--- team work/client/src/components/Community/communityFriends.vue
+++ team work/client/src/components/Community/communityFriends.vue
@@ -63,7 +63,7 @@ export default {
         if (res.data.code == 1) {
           this.list = res.data.data;
           console.log(this.list);
-          // this.loadMore();
+          // this.loadMore();      
         }
       });
     }
